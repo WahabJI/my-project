7c05c046970d6fc55ec3c9a419f37be89b5de5b6
Wahab Javed <wahab.javed@live.com>
2023-02-24 11:57:32 -0600

    Implemented Main page and started projects

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'title': ['Lato', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        // add 2 colors here of a beige color with hex code #FFFFE3 and a blue of #023047
+
+        'beige': '#FFFFE3',
+        'blue': '#023047',
+        'light-blue': 'rgba(173,216,250, 0.65)',
+      },
+    }
+  },
+  plugins: [],
+}
