2424dae85835e8b22912cef4706cda0c729f350e
Wahab Javed <wahab.javed@live.com>
2023-04-22 18:48:15 -0500

    cleaned up the code for Home and Project sections

--- components/sections/HomeSection.tsx
+++ components/sections/HomeSection.tsx
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import { TypeAnimation } from 'react-type-animation';
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import { NavBar } from '../NavBar';
 import { Hamburger } from '../Hamburger';
+import React, {useState, useEffect} from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
 
 export const HomeSection = () => {
@@ -20,71 +20,74 @@ export const HomeSection = () => {
 
     return () => window.removeEventListener('resize', handleResize);
   }, []);
-    return(
-        <section id="home"className="h-screen scroll-smooth bg-gradient-to-b from-cblue to-sky-500">
-            {/* <NavBar /> */}
-            {/* <Hamburger /> */}
-            {isSmallScreen ? <Hamburger /> : <NavBar />}
-            <div className="flex justify-center items-end h-3/6">
-              <h1 className="text-4xl lg:text-6xl font-title font-bold text-beige">Hi, I'm&nbsp; 
-                <TypeAnimation
-                  // Same String at the start will only be typed once, initially
-                  sequence={[
-                  'Wahab',
-                  1000,
-                  'وهب',
-                  1000,
-                  'ワハブ',
-                  1000,
-                  '瓦哈卜',
-                  1000,
-                  'вахаб',
-                  1000,
-                  ]}
-                  speed={10} // Custom Speed from 1-99 - Default Speed: 40
-                  deletionSpeed={40}
-                  wrapper="span" // Animation will be rendered as a <span>
-                  repeat={Infinity} // Repeat this Animation Sequence infinitely
-                  cursor={true} // Hide the Cursor
+
+  return(
+    <section id="home"className="bg-gradient-to-b from-cblue to-sky-500 h-screen">
+      {/* Conditionally render which navbar is being rendered */}
+      {isSmallScreen ? <Hamburger /> : <NavBar />}
+
+      {/* DIV container for the typing animation in the middle of the screen */}
+      <div className="flex justify-center items-end h-3/6">
+        <h1 className="text-4xl lg:text-6xl text-beige font-title font-bold">Hi, I'm&nbsp; 
+          <TypeAnimation
+            sequence={[
+            'Wahab',
+            1000,
+            'وهب',
+            1000,
+            'ワハブ',
+            1000,
+            '瓦哈卜',
+            1000,
+            'вахаб',
+            1000,
+            ]}
+            speed={10} // Custom Speed from 1-99 - Default Speed: 40
+            deletionSpeed={40}
+            wrapper="span" // Animation will be rendered as a <span>
+            repeat={Infinity} // Repeat this Animation Sequence infinitely
+            cursor={true} // Hide the Cursor
+          />
+        </h1>
+      </div>
+      
+      {/* DIV container for the github, linkedin and resume button towards the bottom of the page */}
+      <div className="flex justify-center items-center h-2/6">
+        <div className="grid grid-cols-3 p-4">
+
+          <Link target="_blank" href="https://www.github.com/WahabJI">
+            <div className="flex justify-center items-center w-20 h-20 lg:w-40 lg:h-40 hover:bg-light-blue">
+              <Image 
+                src="/logo-github-light.svg" alt="Github Logo" width={40} height={40}
                 />
-              </h1>
             </div>
+          </Link>
 
-            <div className="flex justify-center items-center h-2/6">
-            {/* <div> tag for the icons at the bottom */}
-              <div className="grid grid-cols-3 p-4">
-
-                <Link target="_blank" href="https://www.github.com/WahabJI">
-                  <div className="flex justify-center items-center w-20 h-20 lg:w-40 w-20 h-20 lg:h-40 hover:bg-light-blue">
-                    <Image 
-                      src="/logo-github-light.svg" alt="Github Logo" width={40} height={40}
-                      />
-                  </div>
-                </Link>
-
-                <Link target="_blank" href="https://www.linkedin.com/in/wahab-javed/">
-                  <div className="flex justify-center items-center  w-20 h-20 lg:w-40 w-20 h-20 lg:h-40 hover:bg-light-blue">
-                    <Image 
-                      src="/logo-linkedin-light.svg" alt="Linkedin Logo" width={40} height={40}
-                      />
-                  </div>
-                </Link>
-
-                <Link target="_blank" href="/RESUME_WJ_01-20-2023.pdf">
-                  <div className="flex justify-center items-center  w-20 h-20 lg:w-40 w-20 h-20 lg:h-40 hover:bg-light-blue">
-                    <Image 
-                      src="/newspaper-outline-light.svg" alt="Resume Logo" width={40} height={40}
-                      />
-                  </div>
-                </Link>
-              </div>
+          <Link target="_blank" href="https://www.linkedin.com/in/wahab-javed/">
+            <div className="flex justify-center items-center w-20 h-20 lg:w-40 lg:h-40 hover:bg-light-blue">
+              <Image 
+                src="/logo-linkedin-light.svg" alt="Linkedin Logo" width={40} height={40}
+                />
             </div>
+          </Link>
 
-            <a href="#projects" className="arrow-container">
-              <div className="arrow"></div>
-              <div className="arrow"></div>
-              <div className="arrow"></div>  
-            </a>      
-          </section> 
-    );
+          <Link target="_blank" href="/RESUME_WJ_01-20-2023.pdf">
+            <div className="flex justify-center items-center w-20 h-20 lg:w-40 lg:h-40 hover:bg-light-blue">
+              <Image 
+                src="/newspaper-outline-light.svg" alt="Resume Logo" width={40} height={40}
+                />
+            </div>
+          </Link>
+
+        </div>
+      </div>
+      
+      {/* anchor tag for the arrow towards the bottom of the page */}
+      <a href="#projects" className="arrow-container">
+        <div className="arrow"></div>
+        <div className="arrow"></div>
+        <div className="arrow"></div>  
+      </a>      
+    </section> 
+  );
 };
--- components/sections/ProjectSection.tsx
+++ components/sections/ProjectSection.tsx
@@ -1,62 +1,64 @@
 import React from 'react';
 
 interface ExperienceSectionProps {
-    myElementRef: React.MutableRefObject<null>;
-    isVisible: boolean;
+  myElementRef: React.MutableRefObject<null>;
+  isVisible: boolean;
 }
 
 export const ProjectSection = (props: ExperienceSectionProps) => {
-    return (
-        <section id="projects" className="bg-gradient-to-b from-sky-500 to-indigo-500 min-h-screen md:h-screen">
-          <div className="h-1/6 items-center flex justify-center">
-            <h1 className="text-6xl font-title font-bold text-beige">Projects</h1>
+  return (
+    <section id="projects" className="bg-gradient-to-b from-sky-500 to-indigo-500 min-h-screen md:h-screen">
+      {/* div container for the title at the top */}
+      <div className="flex justify-center items-center h-1/6">
+        <h1 className="text-6xl text-beige font-title font-bold">Projects</h1>
+      </div>
+
+      <div className="flex items-center my-auto h-5/6">
+        <div className="flex flex-wrap justify-center my-16 mx-auto md:space-x-12 space-y-12 md:space-y-0" ref={props.myElementRef}>
+
+          {/* First div container */}
+          <div className={`bg-white rounded-lg shadow-md w-72 h-80 md:h-96 md:w-96 p-4 transform transition-all duration-500 delay-300 ${props.isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}>
+            <h1 className="bg-gradient-to-r from-cblue to-indigo-500 bg-clip-text text-transparent text-center text-lg md:text-2xl ">StudyUP</h1>
+
+            <div className="flex justify-center items-center mt-4">
+              <hr className="border-gray-300 border-2 rounded-lg w-2/4" />
+            </div>
+
+            <p className="text-justify text-sm md:text-lg px-4 mt-4">
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
           </div>
-            <div className="my-auto items-center flex h-5/6">
 
-              <div className="flex flex-wrap my-16 md:space-x-12 space-y-12 md:space-y-0 mx-auto justify-center" ref={props.myElementRef}>
-                <div className={`bg-white rounded-lg shadow-md p-4 w-72 h-72 md:w-96 w-72 h-80 md:h-96 transform transition-all duration-500 ${props.isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'} delay-300`}>
-                  {/* Card content goes here */}
-                  <h1 className="text-center text-lg md:text-2xl bg-gradient-to-r from-cblue to-indigo-500 bg-clip-text text-transparent">StudyUP</h1>
+          {/* Second div container */}
+          <div className={`bg-white rounded-lg shadow-md w-72 h-80 md:h-96 md:w-96 p-4 transform transition-all duration-500 delay-500 ${props.isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}>
+            <h1 className="bg-gradient-to-r from-cblue to-indigo-500 bg-clip-text text-transparent text-center text-lg md:text-2xl ">Fuel Quoter / Dunya</h1>
 
-                  <div className="flex justify-center items-center mt-4">
-                    <hr className="border-gray-300 border-2 rounded-lg w-2/4" />
-                  </div>
+            <div className="flex justify-center items-center mt-4">
+              <hr className="border-gray-300 border-2 rounded-lg w-2/4" />
+            </div>
 
-                  <p className="px-4 mt-4 text-justify text-sm md:text-lg">
-                    lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                  </p>
-                </div>
+            <p className="text-justify text-sm md:text-lg px-4 mt-4">
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+          </div>
 
+          {/* Third div container */}
+          <div className={`bg-white rounded-lg shadow-md w-72 h-80 md:h-96 md:w-96 p-4 transform transition-all duration-500 delay-700 ${props.isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}>
+            <h1 className="bg-gradient-to-r from-cblue to-indigo-500 bg-clip-text text-transparent text-center text-lg md:text-2xl ">This Website!</h1>
 
-                <div className={`bg-white rounded-lg shadow-md p-4 w-72 h-72 md:w-96 w-72 h-80 md:h-96 transform transition-all duration-500 ${props.isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'} delay-500`}>
-                  {/* Card content goes here */}
-                  <h1 className="text-center text-lg md:text-2xl bg-gradient-to-r from-cblue to-indigo-500 bg-clip-text text-transparent">Fuel Quoter / Dunya</h1>
+            <div className="flex justify-center items-center mt-4">
+              <hr className="border-gray-300 border-2 rounded-lg w-2/4" />
+            </div>
 
-                  <div className="flex justify-center items-center mt-4">
-                    <hr className="border-gray-300 border-2 rounded-lg w-2/4" />
-                  </div>
+            <p className="text-justify text-sm md:text-lg px-4 mt-4">
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+          </div>
 
-                  <p className="px-4 mt-4 text-justify text-sm md:text-lg">
-                    lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                  </p>
-                </div>
-
-
-                <div className={`bg-white rounded-lg shadow-md p-4 w-72 h-72 md:w-96 w-72 h-80 md:h-96 transform transition-all duration-500 ${props.isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'} delay-700`}>
-                  {/* Card content goes here */}
-                  <h1 className="text-center text-lg md:text-2xl bg-gradient-to-r from-cblue to-indigo-500 bg-clip-text text-transparent">This Website!</h1>
-
-                  <div className="flex justify-center items-center mt-4">
-                    <hr className="border-gray-300 border-2 rounded-lg w-2/4" />
-                  </div>
-
-                  <p className="px-4 mt-4 text-justify text-sm md:text-lg">
-                    lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                  </p>
-                </div>
-              </div>
-              </div>
-          </section>
-    );
-    };
+        </div>
+      </div>
+      
+    </section>
+  );
+};
 
