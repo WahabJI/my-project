8956ff7aee51c26d118f528f785df8e46ff42113
Wahab Javed <wahab.javed@live.com>
2023-04-30 02:41:33 -0500

    fixed submit button

--- components/sections/ContactSection.tsx
+++ components/sections/ContactSection.tsx
@@ -7,17 +7,17 @@ export const ContactSection = () => {
         <h1 className="text-4xl md:text-6xl text-beige font-title font-bold">Contact Me!</h1>
       </div>
 
-      <div className="flex justify-center items-center my-auto h-5/6 shadow-lg">
+      <div className="flex justify-center items-center h-5/6 shadow-lg">
         <div className="flex flex-col justify-center items-center bg-white w-5/6 md:w-fit h-fit rounded-lg p-6">
           <form className="">
             <input className="w-full h-12 shadow-md placeholder:text-black focus:ring-16 focus:outline-none focus:ring-4 focus:rounded-md focus:ring-purple-300 placeholder:pl-4 focus:bg-white hover:bg-white rounded-md mb-4" placeholder="Name">
             </input>
             <input className="w-full h-12 shadow-md placeholder:text-black focus:ring-16 focus:outline-none focus:ring-4 focus:rounded-md focus:ring-purple-300 placeholder:pl-4 focus:bg-white hover:bg-white rounded-md mb-4" placeholder="Email">
             </input>
-            <textarea className="w-full h-16 shadow-md placeholder:text-black focus:ring-16 focus:outline-none focus:ring-4 focus:rounded-md focus:ring-purple-300 placeholder:pl-4 placeholder:pt-2 focus:bg-white hover:bg-white rounded-md" placeholder="Message">
+            <textarea className="w-full h-20 shadow-md placeholder:text-black focus:ring-16 focus:outline-none focus:ring-4 focus:rounded-md focus:ring-purple-300 placeholder:pl-4 placeholder:pt-2 focus:bg-white hover:bg-white rounded-md" placeholder="Message">
             </textarea>
             <div className="flex justify-center items-center text-center">
-              <button className="pt-4 p-4 rounded-md shadow-lg">Submit</button>
+              <button className="mt-4 px-4 py-2 text-xl rounded-md shadow-lg ease-in-and-out duration-200 hover:bg-gradient-to-t from-purple-500 to-teal-300 hover:text-white">Submit</button>
             </div>
           </form>
         </div>
