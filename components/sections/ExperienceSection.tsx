256ba6d28bdb1f369f1519de43f6c4f0ac1c8d6e
Wahab Javed <wahab.javed@live.com>
2023-06-23 01:07:31 -0500

    fixed experience section and got rid of scrollbaar for now as it was not working

--- components/sections/ExperienceSection.tsx
+++ components/sections/ExperienceSection.tsx
@@ -10,54 +10,49 @@ interface ExperienceSectionProps {
 export const ExperienceSection = (props: ExperienceSectionProps) => {
     return(
         <section id="experience" className="bg-gradient-to-b from-indigo-500 to-purple-500 min-h-screen flex flex-col overflow-x-hidden">
-            <h1 className="text-center text-6xl font-bold text-beige mt-16">Experience</h1>
-            <div className="grid grid-cols-11 grid-rows-4 pb-32">
+            <h1 className="text-center text-6xl font-bold text-beige my-8">Experience</h1>
+            <div className="grid grid-cols-11 grid-rows-4 pb-32 pt-8">
               
-              <div className="col-span-5 mt-24 flex justify-end" ref={props.myElementRef2}>
+              <div className="col-span-5 flex justify-end items-center" ref={props.myElementRef2}>
                 <div className={`bg-white w-[500px] min-h-44 rounded-lg shadow-md p-6 transform transition-all duration-500 ${!props.isVisible2 ? '-translate-x-full opacity-0' : 'translate-x-0 opacity-100'} ${props.isVisible2? 'delay-100' : ''} `}>
                   {/* Content goes here */}
                   
                   <h1 className="text-3xl bg-gradient-to-r from-indigo-500 to-violet-300 bg-clip-text text-transparent">Plug Power</h1>
-                  <h2 className="text-gray-700 text-xl">Software Engineering Intern</h2>
+                  <h2 className="text-gray-700 text-xl italic">Software Engineering Intern</h2>
                   <ul className="text-lg">
                     <li className="">lorem ipsum dolor sit amet, consectetur adipiscing elit. </li>
                     <li className="">lorem ipsum dolor sit amet, consectetur adipiscing elit. </li>
                     <li className="">lorem ipsum dolor sit amet, consectetur adipiscing elit. </li>
                     <li className="">lorem ipsum dolor sit amet, consectetur adipiscing elit. </li>
                     <li className="">lorem ipsum dolor sit amet, consectetur adipiscing elit. </li>
-                    
                   </ul>
                 </div>
               </div>
               
-              {/* <div className="bg-green-500 rounded-full w-6 h-6"><div className="bg-green-500 rounded-full w-6 h-6 animate-ping"></div></div> */}
-              {/* MIDDLE LINE */}
-              <div className="col-span-1 row-span-4 flex justify-center">
-                <div className="flex justify-center items-center absolute">
-                  <div className="mt-16 h-[1400px] bg-white w-[10px] rounded-lg"></div>
-                </div>
-                <div className={`bg-white shadow-lg rounded-full mt-[115px] absolute transition-all duration-500 delay-100 ${props.isVisible2? 'h-16 w-16 opacity-1' : 'h-4 w-4 opacity-0.5'} flex justify-center items-center` } ><div className="bg-gradient-to-tl shadow-lg from-indigo-500 to-violet-300 w-1/2 h-1/2 rounded-lg rotate-45"><div className="bg-violet-500/80 w-full h-full animate-ping rounded-lg"/></div></div>
-                <div className={`bg-white shadow-lg rounded-full mt-[465px] absolute transition-all duration-500 delay-300 ${props.isVisible2? 'h-16 w-16 opacity-1' : 'h-4 w-4 opacity-0.5'} flex justify-center items-center` }><div className="bg-gradient-to-tl shadow-lg from-indigo-500 to-violet-300 w-1/2 h-1/2 rounded-lg rotate-45"></div></div>
-                <div className={`bg-white shadow-lg rounded-full mt-[810px] absolute transition-all duration-500 delay-200 ${props.isVisible3? 'h-16 w-16 opacity-1' : 'h-4 w-4 opacity-0.5'} flex justify-center items-center` }><div className="bg-gradient-to-tl shadow-lg from-indigo-500 to-violet-300 w-1/2 h-1/2 rounded-lg rotate-45"></div></div>
-                <div className={`bg-white shadow-lg rounded-full mt-[1158px] absolute transition-all duration-500 delay-[400ms] ${props.isVisible3? 'h-16 w-16 opacity-1' : 'h-4 w-4 opacity-0.5'} flex justify-center items-center` }><div className="bg-gradient-to-tl shadow-lg from-indigo-500 to-violet-300 w-1/2 h-1/2 rounded-lg rotate-45"></div></div>
+              <div className="flex justify-center items-center">
+                <div className={`bg-white shadow-lg rounded-full transition-all duration-500 delay-100 z-50 ${props.isVisible2? 'h-16 w-16 opacity-1' : 'h-4 w-4 opacity-0.5'} flex justify-center items-center` } ><div className="bg-gradient-to-tl shadow-lg from-indigo-500 to-violet-300 w-1/2 h-1/2 rounded-lg rotate-45"><div className="bg-violet-500/80 w-full h-full animate-ping rounded-lg"/></div></div>
+                <div className="absolute w-2 h-72 bg-white my-2 rounded-full"></div> 
               </div>
                   
-              <div className="flex justify-start col-span-5 mt-24">
-                <div className="hidden sm:block">
-                  <p className={`text-beige text-center text-2xl mt-7 ${!props.isVisible2 ? 'translate-x-full opacity-0' : 'translate-x-0 opacity-100'} transition-transform duration-500 ${props.isVisible2? 'delay-100' : ''}`}>
+              <div className="flex justify-start col-span-5 items-center">
+                  <p className={`text-beige text-center text-2xl ${!props.isVisible2 ? 'translate-x-full opacity-0' : 'translate-x-0 opacity-100'} transition-transform duration-500 ${props.isVisible2? 'delay-100' : ''}`}>
                     Month Year - Month Year
                   </p>
-                  </div>
               </div>
 
 
-              <div className="col-span-5 mt-24 flex justify-end">
-                <p className={`text-beige text-center text-2xl mt-7 ${!props.isVisible2 ? '-translate-x-full opacity-0' : 'translate-x-0 opacity-100'} transition-transform duration-500 ${props.isVisible2? 'delay-300' : ''}`}>
+              <div className="col-span-5 flex justify-end items-center">
+                <p className={`text-beige text-center text-2xl ${!props.isVisible2 ? '-translate-x-full opacity-0' : 'translate-x-0 opacity-100'} transition-transform duration-500 ${props.isVisible2? 'delay-300' : ''}`}>
                   Month Year - Month Year
                 </p>
               </div>
 
-              <div className="col-span-5 mt-24 flex justify-start">
+              <div className="flex justify-center items-center">
+                <div className={`bg-white shadow-lg rounded-full transition-all duration-500 delay-100 z-50 ${props.isVisible2? 'h-16 w-16 opacity-1' : 'h-4 w-4 opacity-0.5'} flex justify-center items-center` } ><div className="bg-gradient-to-tl shadow-lg from-indigo-500 to-violet-300 w-1/2 h-1/2 rounded-lg rotate-45"><div className="bg-violet-500/80 w-full h-full animate-ping rounded-lg"/></div></div>
+                <div className="absolute w-2 h-80 bg-white my-2 rounded-full"></div> 
+              </div>
+
+              <div className="col-span-5 flex justify-start items-center">
                 <div className={`bg-white w-[500px] min-h-44 rounded-lg shadow-md p-6 transform transition-all duration-500 ${!props.isVisible2 ? ' translate-x-full opacity-0' : 'translate-x-0 opacity-100'} ${props.isVisible2? 'delay-300' : ''} `}>
                   {/* Content goes here */}
                   <h1 className="text-3xl bg-gradient-to-r from-indigo-500 to-violet-300 bg-clip-text text-transparent">University of Houston | Computer Science Department</h1>
@@ -77,7 +72,7 @@ export const ExperienceSection = (props: ExperienceSectionProps) => {
 
 
               
-              <div className="col-span-5 mt-24 flex justify-end">
+              <div className="col-span-5 flex justify-end items-center">
                 <div className={`bg-white w-[500px] min-h-44 rounded-lg shadow-md p-6 transform transition-all duration-500 ${!props.isVisible3 ? '-translate-x-full opacity-0' : 'translate-x-0 opacity-100'} ${props.isVisible3? 'delay-300' : ''} `}>
                   {/* Content goes here */}
                   <h1 className="text-3xl bg-gradient-to-r from-indigo-500 to-violet-300 bg-clip-text text-transparent" ref={props.myElementRef3}>DM Clinical Research</h1>
@@ -91,20 +86,30 @@ export const ExperienceSection = (props: ExperienceSectionProps) => {
                   </ul>
                 </div>
               </div>
-                  
-              <div className="col-span-5 mt-24 flex justify-start">
-                <p className={`text-beige text-center text-2xl mt-7 ${!props.isVisible3 ? 'translate-x-full opacity-0' : 'translate-x-0 opacity-100'} transition-transform duration-500 ${props.isVisible3? 'delay-300' : ''}`}>
+              
+              <div className="flex justify-center items-center">
+                <div className={`bg-white shadow-lg rounded-full transition-all duration-500 delay-100 z-50 ${props.isVisible2? 'h-16 w-16 opacity-1' : 'h-4 w-4 opacity-0.5'} flex justify-center items-center` } ></div>
+                <div className="absolute w-2 h-80 bg-white my-2 rounded-full"></div> 
+              </div>
+
+              <div className="col-span-5 flex justify-start items-center">
+                <p className={`text-beige text-center text-2xl ${!props.isVisible3 ? 'translate-x-full opacity-0' : 'translate-x-0 opacity-100'} transition-transform duration-500 ${props.isVisible3? 'delay-300' : ''}`}>
                   Month Year - Month Year
                 </p>
               </div>
 
-              <div className="col-span-5 mt-24 flex justify-end">
-                <p className={`text-beige text-center text-2xl mt-7 ${!props.isVisible3 ? '-translate-x-full opacity-0' : 'translate-x-0 opacity-100'} transition-transform duration-500 ${props.isVisible3? 'delay-500' : ''}`}>
+              <div className="col-span-5 flex justify-end items-center">
+                <p className={`text-beige text-center text-2xl ${!props.isVisible3 ? '-translate-x-full opacity-0' : 'translate-x-0 opacity-100'} transition-transform duration-500 ${props.isVisible3? 'delay-500' : ''}`}>
                   Month Year - Month Year
                 </p>
               </div>
 
-              <div className="col-span-5 mt-24 flex justify-start">
+              <div className="flex justify-center items-center">
+                <div className={`bg-white shadow-lg rounded-full transition-all duration-500 delay-100 z-50 ${props.isVisible2? 'h-16 w-16 opacity-1' : 'h-4 w-4 opacity-0.5'} flex justify-center items-center` } ></div>
+                <div className="absolute w-2 h-72 bg-white my-2 rounded-full"></div> 
+              </div>
+
+              <div className="col-span-5 flex justify-start items-center">
                 <div className={`bg-white w-[500px] min-h-44 rounded-lg shadow-md p-6 transform transition-all duration-500 ${!props.isVisible3 ? 'translate-x-full opacity-0' : 'translate-x-0 opacity-100'} ${props.isVisible3? 'delay-500' : ''} `}>
                   {/* Content goes here */}
                   <h1 className="text-3xl bg-gradient-to-r from-indigo-500 to-violet-300 bg-clip-text text-transparent" ref={props.myElementRef3}>The UPS Store #6735</h1>
