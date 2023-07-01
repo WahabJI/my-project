a2537663a2bbc531a3c672b8574298d9723211bb
Wahab Javed <wahab.javed@live.com>
2023-07-01 17:58:08 -0500

    small error where title wasn't italicised

--- components/sections/ExperienceSectionResponsive.tsx
+++ components/sections/ExperienceSectionResponsive.tsx
@@ -31,7 +31,7 @@ export const ExperienceSectionResponsive = (props: ExperienceSectionProps) => {
                 </div>
                 <div className="bg-white p-4 shadow-md min-h-40 mt-4 rounded-xl col-span-10">
                     <h2 className="text-lg font-bold">UH Computer Science Department <span className="text-gray-900 font-normal">| January 2023 - May 2023</span></h2>
-                    <p className="text-gray-700">Academic Support Assistant</p>
+                    <p className="text-gray-700 italic">Academic Support Assistant</p>
                     <p className="">I worked with the course intructor to deliver impactful instructions to over 150 students. I also held dedicated office hours to provide constructive feedback and comprehensive code reviews to ensure continuous growth.</p>
                 </div>
                 <div className="flex justify-center items-center col-span-2">
@@ -40,7 +40,7 @@ export const ExperienceSectionResponsive = (props: ExperienceSectionProps) => {
                 </div>
                 <div className="bg-white p-4 shadow-md min-h-40 mt-4 rounded-xl col-span-10">
                     <h2 className="text-lg font-bold">DM Clinical Research <span className="text-gray-900 font-normal">| September 2022 - December 2022</span></h2>
-                    <p className="text-gray-700">IT Intern</p>
+                    <p className="text-gray-700 italic">IT Intern</p>
                     <p className="">Together with the IT team, we deployed a streamlined ticketing system that significantly improved issue resolution efficiency. Additionally, I played a crucial role in facilitating the smooth onboarding of over 30 new employees each month, while providing exceptional level 1 technical support.</p>
                 </div>
                 <div className="flex justify-center items-center col-span-2">
@@ -49,7 +49,7 @@ export const ExperienceSectionResponsive = (props: ExperienceSectionProps) => {
                 </div>
                 <div className="bg-white p-4 shadow-md min-h-40 mt-4 rounded-xl col-span-10">
                     <h2 className="text-lg font-bold">The UPS Store #6735 <span className="text-gray-900 font-normal">| April 2021 - August 2022</span></h2>
-                    <p className="text-gray-700">Center Manager</p>
+                    <p className="text-gray-700 italic">Center Manager</p>
                     <p className="">Led operations, fostering vendor relationships and ensuring customer satisfaction during a seamless franchise transition. I also played a key role in team recruitment and management to drive outstanding results.</p>
                 </div>
                 {/* Add more job cards as needed */}
